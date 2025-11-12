516a5ad08af49f9cfbb2d03a391ea8731c31c36a
mahmoodCod <nmashoomy11@gmail.com>
2025-11-12 14:07:19 +0330

    feat(auth): implement send OTP controller
    - Implement send controller with validation
    - Add phone number validation using sendOtpValidator
    - Add OTP expiration check before sending new OTP
    - Add remaining time display for existing OTP
    - Add OTP generation and SMS sending
    - Add errorResponse and successRespons imports
    - Add Ban model check (commented until model is created)
    - Integrate with OTP service and validators
    - Add proper error handling and response messages

--- controller/v1/auth.js
+++ controller/v1/auth.js
@@ -1,5 +1,10 @@
 const redis = require('../../redis');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { sendSms } = require('../../services/otp');
+const { sendOtpValidator } = require('../../validator/auth');
+const { errorResponse, successRespons } = require('../../helpers/responses');
+// const Ban = require('../../model/Ban'); // TODO: Create Ban model if needed
 
 function getOtpRedisPattern(phone) {
     return `OTP: ${phone}`;
@@ -43,7 +48,28 @@ const genarateOtp = async(phone, length = 6, expireTime = 2) => {
 
 exports.send = async (req,res,next) => {
     try {
+        const { phone } = req.body;
 
+        await sendOtpValidator.validate(req.body, {abortEarly: false});
+
+        // TODO: Uncomment when Ban model is created
+        // const Ban = require('../../model/Ban');
+        // const isBanned = await Ban.findOne({ phone });
+        // if (isBanned) {
+        //     return errorResponse(res, 403, 'This phone number has been banned');
+        // };
+
+        const { expired,remainingTime } = await getOtpDetails(phone);
+
+        if (!expired) {
+            return successRespons(res, 200, {message: `OTP already send, please try again afetr ${remainingTime}`});
+        };
+
+        const otp = await genarateOtp(phone);
+
+        await sendSms(phone,otp);
+
+        return successRespons(res,200,{message: 'otp send successfully :))'});
     } catch (err) {
         next(err);
     };
