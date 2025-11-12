5dcc62f60a87f10576588071f472e0a0b1992fb4
mahmoodCod <nmashoomy11@gmail.com>
2025-11-12 13:51:13 +0330

    feat(auth): add authentication controllers and connect to routes
    - Create controller/v1/auth.js with authentication controllers
    - Add send controller for sending OTP
    - Add verify controller for verifying OTP
    - Add getMe controller for getting current user info
    - Connect controllers to routes in routes/v1/auth.js
    - Add authentication middleware to /me route
    - Import controllers and middleware in routes
    - Setup controller structure for OTP authentication flow

---
+++ controller/v1/auth.js
@@ -0,0 +1,25 @@
+
+exports.send = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.verify = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.getMe = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
