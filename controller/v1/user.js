1430b630de6531affe7cf1ab58bdddeacaa4ef95
mahmoodCod <nmashoomy11@gmail.com>
2025-11-12 20:48:00 +0330

    feat(controller): add user controller with initial structure
    Create user controller module with function stubs for user management operations.
    This establishes the foundation for implementing user-related business logic.
    
    - Add getAll function for retrieving all users
    
    - Add banUser function for banning users
    
    - Add createAddress function for creating user addresses
    
    - Add removeAddress function for removing user addresses
    
    - Add updateAddress function for updating user addresses

---
+++ controller/v1/user.js
@@ -0,0 +1,42 @@
+
+
+exports.getAll = async(req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.banUser = async(req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.createAddress = async(req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.removeAddress = async(req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.updateAddress = async(req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
