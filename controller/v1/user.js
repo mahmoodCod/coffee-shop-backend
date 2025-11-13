7661f41364c56eeda82a7b051ad28475a4eae024
mahmoodCod <nmashoomy11@gmail.com>
2025-11-13 09:19:02 +0330

    feat(controller): implement createAddress function with validation
    Add validation and request handling logic to createAddress function.
    Import necessary validators and cities data for address creation.
    
    - Import createAddressValidator from address validator
    
    - Import cities data for address validation
    
    - Extract user from request object
    
    - Destructure address fields from request body
    
    - Add validation for address creation request

--- controller/v1/user.js
+++ controller/v1/user.js
@@ -1,4 +1,5 @@
-
+const { createAddressValidator } = require('../../validator/address');
+const cities = require('../../cities/')
 
 exports.getAll = async(req,res,next) => {
     try {
@@ -18,6 +19,11 @@ exports.banUser = async(req,res,next) => {
 
 exports.createAddress = async(req,res,next) => {
     try {
+        const user = req.user;
+
+        const { name, postalCode, province, city, street } = req.body;
+
+        await createAddressValidator.validate( req.body, { abortEarly: false } );
 
     } catch (err) {
         next(err);
