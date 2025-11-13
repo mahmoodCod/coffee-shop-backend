62d3b95aa2fee5142d00f8888f265c968215c49b
mahmoodCod <nmashoomy11@gmail.com>
2025-11-13 09:42:50 +0330

    feat(controller): complete createAddress implementation with database save
    Complete the createAddress function implementation by adding database operations
    to save address to user document. Add success response and User model import.
    
    - Add successRespons import for success response handling
    
    - Add User model import for database operations
    
    - Create addressObject from request body fields
    
    - Save address to user document using findByIdAndUpdate with $push
    
    - Return success response with updated user data

--- controller/v1/user.js
+++ controller/v1/user.js
@@ -1,6 +1,7 @@
 const { createAddressValidator } = require('../../validator/address');
 const cities = require('../../cities/cities.json');
-const { errorResponse } = require('../../helpers/responses');
+const { errorResponse, successRespons } = require('../../helpers/responses');
+const User = require('../../model/User');
 
 exports.getAll = async(req,res,next) => {
     try {
@@ -30,6 +31,26 @@ exports.createAddress = async(req,res,next) => {
         if (!cityData) {
             return errorResponse(res,409, "City is not valid !!");
         };
+
+        const addressObject = {
+            name,
+            postalCode,
+            province,
+            city,
+            street
+        };
+
+        const updatedUser = await User.findByIdAndUpdate( user._id, {
+            $push: {
+                addresses: addressObject,
+            },
+        },
+        {
+            new: true,
+        },
+    );
+
+    return successRespons(res,201, { user: updatedUser, message:'Address create successfully :))'});
 
     } catch (err) {
         next(err);
