9cc75d3d9698de29c84574daca74152f707e118a
mahmoodCod <nmashoomy11@gmail.com>
2025-11-18 15:04:43 +0330

    Product: add basic schema fields
    - Add name, slug, description fields
    
    - Add category reference to Category model
    
    - Set required and unique constraints

--- model/Product.js
+++ model/Product.js
@@ -1,6 +1,29 @@
 const mongoose = require('mongoose');
 
-const productSchema = new mongoose.Schema({});
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    slug: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        trime: true,
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+        required: true,
+    },
+    
+});
 
 const product = mongoose.model('Product', productSchema);
 
