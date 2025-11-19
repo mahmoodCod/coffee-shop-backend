99b3c1390102b956c1d97d53614418830b28143d
mahmoodCod <nmashoomy11@gmail.com>
2025-11-19 19:19:56 +0330

    Order: add initial order controller structure
    - Create controller/v1/order.js file
    
    - Add getAllOrders function with empty implementation
    
    - Add updateOrder function with empty implementation
    
    - Export both functions for use in routes

---
+++ controller/v1/order.js
@@ -0,0 +1,16 @@
+
+exports.getAllOrders = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next (err);
+    };
+};
+
+exports.updateOrder = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next (err);
+    };
+};
