2852492b551e6242bbb4cf693249e7d50a17f767
mahmoodCod <nmashoomy11@gmail.com>
2025-11-20 00:39:33 +0330

    Article: add article controller structure
    - Add getAllArticles function skeleton
    
    - Add createArticle function skeleton
    
    - Add getOne function skeleton
    
    - Add deleteArticle function skeleton
    
    - Add updateArticle function skeleton
    
    - Add saveDraft function skeleton

---
+++ controller/v1/article.js
@@ -0,0 +1,48 @@
+
+exports.getAllArticles = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next (err);
+    };
+};
+
+exports.createArticle = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next (err);
+    };
+};
+
+exports.getOne = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next (err);
+    };
+};
+
+exports.deleteArticle = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next (err);
+    };
+};
+
+exports.updateArticle = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next (err);
+    };
+};
+
+exports.saveDraft = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next (err);
+    };
+};
