dd207ab83eebb1de2da612a2d39782d7739958b9
mahmoodCod <nmashoomy11@gmail.com>
2025-11-19 19:22:07 +0330

    Order: add initial getAllOrders implementation
    - Add pagination with page and limit query parameters
    
    - Get user from request
    
    - Add filters object with conditional user filter (ADMIN can see all orders)

--- controller/v1/order.js
+++ controller/v1/order.js
@@ -1,6 +1,12 @@
 
 exports.getAllOrders = async (req,res,next) => {
     try {
+        const { page = 1, limit = 10 } = req.query;
+        const user = req.user;
+
+        const filters = {
+            ...(user.roles.includes("ADMOIN") ? {} : { user: user._id }),
+        };
 
     } catch (err) {
         next (err);
