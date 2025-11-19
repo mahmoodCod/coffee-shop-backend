069d455cf5eef317622ad92345250526b2fba90d
mahmoodCod <nmashoomy11@gmail.com>
2025-11-19 11:10:12 +0330

    Comment: add replies field and export model
    - Add replies array field for nested comments
    
    - Export Comment model for use in other modules

--- model/Comment.js
+++ model/Comment.js
@@ -21,6 +21,9 @@ const commentSchema = new mongoose.Schema({
         ref: 'User',
         required: true,
     },
+    replies: [],
 }, { timestamps: true });
 
 const comment = mongoose.model('Comment', commentSchema);
+
+module.exports = comment;
