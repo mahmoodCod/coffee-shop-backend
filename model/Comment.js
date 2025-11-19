71c522582619beaa7095e1a357bcfdf146d322c9
mahmoodCod <nmashoomy11@gmail.com>
2025-11-19 10:56:47 +0330

    Comment: add basic schema fields
    - Add content field (required string)
    
    - Add product reference (required ObjectId)
    
    - Add rating field (1-5, required number)
    
    - Add user reference (required ObjectId)
    
    - Add timestamps for createdAt and updatedAt

--- model/Comment.js
+++ model/Comment.js
@@ -1,6 +1,26 @@
 const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema({
-});
+    content: {
+        type: String,
+        required: true,
+    },
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true,
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+}, { timestamps: true });
 
 const comment = mongoose.model('Comment', commentSchema);
