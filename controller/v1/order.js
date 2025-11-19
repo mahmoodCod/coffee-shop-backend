db1a0612503d13213b585697f5d5a6b466966d74
mahmoodCod <nmashoomy11@gmail.com>
2025-11-19 19:29:55 +0330

    Order: add updateOrder implementation
    - Import updateOrderValidator
    
    - Get postTrackingCode and status from request body
    
    - Get id from request params
    
    - Validate request body with updateOrderValidator
    
    - Update order using findByIdAndUpdate
    
    - Return success response with updated order

--- controller/v1/order.js
+++ controller/v1/order.js
@@ -1,6 +1,7 @@
 const { successRespons } = require("../../helpers/responses");
 const Order = require("../../model/Order");
 const { createPaginationData } = require("../../utils");
+const { updateOrderValidator } = require('../../validator/order');
 
 exports.getAllOrders = async (req,res,next) => {
     try {
@@ -27,6 +28,20 @@ exports.getAllOrders = async (req,res,next) => {
 
 exports.updateOrder = async (req,res,next) => {
     try {
+        const { postTrackingCode, status } = req.body;
+        const { id } = req.params;
+
+        await updateOrderValidator.validate(req.body, { abortEarly: false });
+
+        const order =  await Order.findByIdAndUpdate({
+            postTrackingCode,
+            status
+        },{ new: true });
+
+        return successRespons(res,200, { 
+            order,
+            message: 'Order updated successfully :))'
+        });
 
     } catch (err) {
         next (err);
