5411583835d7aab5d332fbaceaa82257a884a090
mahmoodCod <nmashoomy11@gmail.com>
2025-11-19 19:14:25 +0330

    Order: add routes for getAllOrders and updateOrder
    - Import auth middleware and roleGuard
    
    - Import getAllOrders and updateOrder from controller
    
    - Add GET / route with auth middleware for getAllOrders
    
    - Add PATCH /:id route with auth and ADMIN roleGuard for updateOrder

--- routes/v1/order.js
+++ routes/v1/order.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const { auth } = require('../../middleware/auth');
+const roleGuard = require('../../middleware/roleGuard');
+const { getAllOrders, updateOrder } = require('../../controller/v1/order');
 
 const router = express.Router();
+
+router.route("/").get(auth, getAllOrders);
+router.route("/:id").patch(auth, roleGuard('ADMIN'), updateOrder);
 
 module.exports = router;
