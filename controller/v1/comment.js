50655ef2d188168a4a0b9d3d0f466f6b08a52ad5
mahmoodCod <nmashoomy11@gmail.com>
2025-11-19 12:27:47 +0330

    Comment: add initial comment controller structure
    - Create empty controller functions for comment operations
    
    - Add createComment, getComment, getAllComments functions
    
    - Add removeComment, updateComment functions
    
    - Add createReply, updateReply, removeReply functions

---
+++ controller/v1/comment.js
@@ -0,0 +1,64 @@
+
+exports.createComment = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.getComment = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.getAllComments = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.removeComment = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.updateComment = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.createReply = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.updateReply = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.removeReply = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
