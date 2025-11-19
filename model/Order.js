609480e91b8fc1507994c62b6048ff2327c5e79d
mahmoodCod <nmashoomy11@gmail.com>
2025-11-19 19:03:18 +0330

    Order: add items array and orderItemSchema
    - Create orderItemSchema with product, quantity, and priceAtTimeOfAdding fields
    
    - Add items array to orderSchema using orderItemSchema
    
    - Add module.exports for Order model

--- model/Order.js
+++ model/Order.js
@@ -1,11 +1,29 @@
 const mongoose = require('mongoose');
 
+const orderItemSchema = new mongoose.Schema({
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        min: 1
+    },
+    priceAtTimeOfAdding: {
+        type: Number,
+        required: true
+    },
+});
+
 const orderSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
     },
+    items: [orderItemSchema],
     shippingAddress: {
         postalCode: {
             type: String,
@@ -52,3 +70,5 @@ orderSchema.virtual("totalPrice").get(function() {
 });
 
 const order = mongoose.model('Order', orderSchema);
+
+module.exports = order;
