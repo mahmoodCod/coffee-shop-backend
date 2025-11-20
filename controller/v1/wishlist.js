f102477f7ef70040526572d14c9a2b831369a1fd
mahmoodCod <nmashoomy11@gmail.com>
2025-11-20 11:04:38 +0330

    Wishlist: implement addToWishlist controller
    Complete addToWishlist function implementation:
    
    - Add required imports (responses, validator, models)
    
    - Validate request body with addToWishlistValidator
    
    - Check if product exists before adding
    
    - Create wishlist entry with user and product
    
    - Return success response with created favorite

--- controller/v1/wishlist.js
+++ controller/v1/wishlist.js
@@ -1,6 +1,28 @@
+const { successRespons, errorResponse } = require("../../helpers/responses");
+const { addToWishlistValidator } = require('../../validator/wishlist');
+const Wishlist = require("../../model/Wishlist");
+const Product = require('../../model/Product');
 
 exports.addToWishlist = async (req,res,next) => {
     try {
+        const user = req.user._id;
+
+        await addToWishlistValidator.validate(req.body, { abortEarly: false });
+
+        const { product } = req.body;
+
+        const exists = await Product.findById(product);
+            if (!exists) return errorResponse(res, 404, "Product not found");
+
+        const favorite = await Wishlist.create({
+            user,
+            product
+        });
+
+        return successRespons(res, 201, {
+            message: "Added to wishlist",
+            favorite
+        });
 
     } catch (err) {
         next(err);
