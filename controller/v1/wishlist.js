11f0f2584623978b9c83a83649f9313fa8540a1e
mahmoodCod <nmashoomy11@gmail.com>
2025-11-20 10:47:08 +0330

    Wishlist: add wishlist controller structure
    Create controller skeleton for wishlist operations:
    
    - addToWishlist - Add product to user's wishlist
    
    - getWishlist - Get user's wishlist items
    
    - removeFromWishlist - Remove product from wishlist
    
    - All functions include error handling with next(err)

---
+++ controller/v1/wishlist.js
@@ -0,0 +1,24 @@
+
+exports.addToWishlist = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.getWishlist = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.removeFromWishlist = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
