049dcd639d1d707562cf12e6b0d609252a9026cb
mahmoodCod <nmashoomy11@gmail.com>
2025-11-21 22:00:50 +0330

    DiscountCode: create controller structure with handler functions
    Add discount code controller with initial handlers:
    
    - Add createDiscountCode, getAllDiscountCode, getOneDiscountCode handlers
    
    - Add updateDiscountCode, deleteDiscountCode, applyDiscountCode handlers
    
    - Set up async/await structure with error handling

---
+++ controller/v1/discountCode.js
@@ -0,0 +1,48 @@
+
+exports.createDiscountCode = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.getAllDiscountCode = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.getOneDiscountCode = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.updateDiscountCode = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.deleteDiscountCode = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.applyDiscountCode = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
