32dd403a95f85af317acc47b36df47bd3e2738f3
mahmoodCod <nmashoomy11@gmail.com>
2025-11-23 00:39:47 +0330

    BankAccount: add controller structure
    Add BankAccount controller:
    
    - Add createBankAccount function structure
    
    - Add getAllBankAccount function structure
    
    - Add getOneBankAccount function structure
    
    - Add updateBankAccount function structure
    
    - Add deleteBankAccount function structure

---
+++ controller/v1/bankAccount.js
@@ -0,0 +1,40 @@
+
+exports.createBankAccount = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next (err);
+    };
+};
+
+exports.getAllBankAccount = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next (err);
+    };
+};
+
+exports.getOneBankAccount = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next (err);
+    };
+};
+
+exports.updateBankAccount = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next (err);
+    };
+};
+
+exports.deleteBankAccount = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next (err);
+    };
+};
