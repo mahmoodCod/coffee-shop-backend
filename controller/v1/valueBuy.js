3f10a2b39b23a72b55555f2e580c838cb12943ff
mahmoodCod <nmashoomy11@gmail.com>
2025-11-22 15:07:42 +0330

    ValueBuy: add controller structure
    Add ValueBuy controller:
    
    - Add createValueBuy function structure
    
    - Add getAllValueBuy function structure
    
    - Add getOneValueBuy function structure
    
    - Add updateValueBuy function structure
    
    - Add deleteValueBuy function structure

---
+++ controller/v1/valueBuy.js
@@ -0,0 +1,40 @@
+
+exports.createValueBuy = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.getAllValueBuy = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.getOneValueBuy = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.updateValueBuy = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.deleteValueBuy = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
