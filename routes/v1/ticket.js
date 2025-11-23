d71468b490393f3d61db003d9ebafac0de51e32a
mahmoodCod <nmashoomy11@gmail.com>
2025-11-23 09:50:48 +0330

    Ticket: implement ticket routes
    Add ticket routes with:
    
    - POST /api/v1/ticket (auth required)
    
    - GET /api/v1/ticket (auth required)
    
    - GET /api/v1/ticket/:id (auth required)
    
    - PATCH /api/v1/ticket/:id (auth required)
    
    - DELETE /api/v1/ticket/:id (auth required)

--- routes/v1/ticket.js
+++ routes/v1/ticket.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const { auth } = require('../../middleware/auth');
+const { createTicket, getAllTicket, getOneTicket, updateTicket, deleteTicket } = require('../../controller/v1/ticket');
 
 const router = express.Router();
+
+router.route('/')
+    .post(auth, createTicket)
+    .get(auth, getAllTicket);
+
+router.route('/:id')
+    .get(auth, getOneTicket)
+    .patch(auth, updateTicket)
+    .delete(auth, deleteTicket);
 
 module.exports = router;
