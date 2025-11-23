dda2e773bdbf00bbd3b52fe4f2437a0c4c8ac1f9
mahmoodCod <nmashoomy11@gmail.com>
2025-11-23 10:05:13 +0330

    Ticket: add ticket controller structure
    Add ticket controller with function stubs:
    
    - createTicket
    
    - getAllTicket
    
    - getOneTicket
    
    - updateTicket
    
    - deleteTicket
    
    - getMyTickets
    
    - replyTicket
    
    - closeTicket

---
+++ controller/v1/ticket.js
@@ -0,0 +1,64 @@
+
+exports.createTicket = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.getAllTicket = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.getOneTicket = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.updateTicket = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.deleteTicket = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.getMyTickets = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.replyTicket = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.closeTicket = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
