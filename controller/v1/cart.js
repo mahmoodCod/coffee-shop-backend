9457eb564fff3bbf7b1f8d5fb383f1550f665b8e
mahmoodCod <nmashoomy11@gmail.com>
2025-11-24 19:53:37 +0330

    Cart: implement addCart controller function
    - Validate request body using addToCartValidator
    - Check if productId is a valid ObjectId
    - Find product in database and ensure it exists
    - Create a new cart if user has no existing cart
    - If cart exists, check if product is already in items
      - If yes, increment quantity
      - If no, push new product with priceAtTimeOfAdding
    - Save the cart after modifications
    - Return success response with updated cart
    - Handle errors with try/catch and pass to next middleware

--- controller/v1/cart.js
+++ controller/v1/cart.js
@@ -1,3 +1,7 @@
+const { errorResponse, successRespons } = require('../../helpers/responses');
+const {addToCartValidator} = require('../../validator/cart');
+const Product = require('../../model/Product');
+const Cart = require('../../model/Cart');
 
 exports.getCart = async (req,res,next) => {
     try {
@@ -9,7 +13,64 @@ exports.getCart = async (req,res,next) => {
 
 exports.addCart = async (req,res,next) => {
     try {
+        await addToCartValidator.validate(req.body, { abortEarly: false });
+        
+        const user = req.user;
+        const { productId, quantity } = req.body;
 
+        if (!isValidObjectId(productId)){
+            return errorResponse(res,400, 'Product is not valid !!');
+        };
+
+        const product = await Product.findById(productId);
+        if (!product) {
+            return errorResponse(res,404, 'Product not found !!');
+        };
+
+        const productDetails = product;
+
+        if (!productDetails) {
+            return errorResponse(res,400, 'Product does not sell !!');
+        };
+
+        const cart = await Cart.findOne({ user: user._id });
+        const priceAtTimeOfAdding = productDetails.price;
+
+        if (!cart) {
+            const newCart = await Cart.create({
+                user: user._id,
+                items: [
+                    {
+                        product: productId,
+                        quantity,
+                        priceAtTimeOfAdding
+                    },
+                ],
+            });
+
+            return successRespons(res,200, {
+                cart: newCart
+            });
+        };
+
+        const existingItem = cart.items.find((item) => {
+            return item.product.toString() === productId ;
+        });
+
+        if (existingItem) {
+            existingItem.quantity += quantity;
+            // existingItem.priceAtTimeOfAdding += priceAtTimeOfAdding;
+        } else {
+            cart.items.push({
+                product: productId,
+                quantity,
+                priceAtTimeOfAdding,
+            });
+        };
+
+        await cart.save();
+
+        return successRespons(res,200, { cart });
     } catch (err) {
         next(err);
     };
