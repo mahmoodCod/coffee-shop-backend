b60841a82a5198ba4a274d954c09ba98654d18ec
mahmoodCod <nmashoomy11@gmail.com>
2025-11-25 10:39:43 +0330

    feat(checkout): scaffold checkout controller with CRUD handlers
    - Create empty async functions for createCheckout, getAllCheckouts, getCheckout, updateCheckout, removeCheckout
    - Add try/catch blocks with error forwarding to next middleware
    - Prepare controller for future business logic implementation

---
+++ controller/v1/checkout.js
@@ -0,0 +1,40 @@
+
+exports.createCheckout = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.getAllCheckouts = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.getCheckout = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.updateCheckout = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
+
+exports.removeCheckout = async (req,res,next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    };
+};
